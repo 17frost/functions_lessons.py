3908c3870a40db3186dd0c4ede538f3882186fe3
17frost <144569847+17frost@users.noreply.github.com>
2026-01-26 23:28:52 -0600

    Update 1_.py

--- 1_.py
+++ 1_.py
@@ -1,3 +1,48 @@
+# def happy_birthday(name, age):
+#     print(f"Happy birthday to you {name}!")
+#     print(f"You are {age} years old")
+#     print("Happy birthday to you")
+#     print()
+
+# happy_birthday("Jeffery Epstein", 67)
+# happy_birthday("Bibi Netanyahu", 67)
+# happy_birthday("trump", 67)
+
+# def display_invoice(username, amount, due_date):
+#     print(f"Hello {username}")
+#     print(f"Your bill of {amount} is due on {due_date}")
+
+# display_invoice("Epstein", "$100000000", "6/07")
+
+# def add(x, y):
+#     z = x + y
+#     return z
+
+# def subtract(x, y):
+#     z = x - y
+#     return z
+
+# def multiply(x, y):
+#     z = x * y
+#     return z
+
+# def divide(x, y):
+#     z = x / y
+#     return z
+
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(multiply(1, 2))
+# print(divide(1, 2))
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("spongebob", "squarepants")
+print(full_name)
+
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
 
